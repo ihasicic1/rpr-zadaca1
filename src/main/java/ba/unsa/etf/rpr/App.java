dc0018591e4051e8f21cb054e6ff6a30c7a97866
ihasicic1 <ihasicic1@etf.unsa.ba>
2022-11-17 11:08:18 +0100

    html generated javadoc added

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -1,15 +1,12 @@
 package ba.unsa.etf.rpr;
 
-import java.util.Scanner;
-import java.util.Stack;
-
 public class App {
     public static void main( String[] args ) {
         String s = args[0];
         ExpressionEvaluator expression = new ExpressionEvaluator();
         if(s.isEmpty()) System.out.println("Empty string");
         else {
-            System.out.println(Math.round(expression.evaluate(s)*100.) / 100.); //
+            System.out.println(Math.round(expression.evaluate(s)*100.) / 100.);
         }
     }
 }
--- src/main/java/ba/unsa/etf/rpr/ExpressionEvaluator.java
+++ src/main/java/ba/unsa/etf/rpr/ExpressionEvaluator.java
@@ -2,55 +2,54 @@ package ba.unsa.etf.rpr;
 
 import java.util.Stack;
 
-public class ExpressionEvaluator {
+public class ExpressionEvaluator {       //class that implements Dijkstra algorithm
     private final Stack<String> operators = new Stack<>();
     private final Stack<Double> operands = new Stack<>();
 
-    private boolean checkSqrt(String s, int i){
+    private boolean checkSqrt(String s, int i){     //method for checking if input string has operator sqrt
         return s.substring(i, i+4).equals("sqrt");
     }
 
-    protected static int numberLength(String s, int i){
+    protected static int numberLength(String s, int i){     //method that calculates length of an operand, a dot '.' is also considered +1
         int counter = 0;
         for(int j = i; j < s.length(); j++){
-            if(s.charAt(j) == ' ') break;
+            if(s.charAt(j) == ' ') break;                //skips spaces, counts digits and dots
             counter++;
-
         }
         return counter;
     }
 
-    public Double evaluate(String s){
+    public Double evaluate(String s){  //method for evaluating the expression
         for(int i = 0; i < s.length(); i++){
             if(s.charAt(i) == '(');
             else if(s.charAt(i) == '+') operators.push("+");
             else if(s.charAt(i) == '-') operators.push("-");
             else if(s.charAt(i) == '*') operators.push("*");
             else if(s.charAt(i) == '/') operators.push("/");
-            else if(s.charAt(i) == 's' && checkSqrt(s, i)) {operators.push("sqrt"); i = i + 4;}
+            else if(s.charAt(i) == 's' && checkSqrt(s, i)) {operators.push("sqrt"); i = i + 4;} //if "s", check if it equals "sqrt", increase i by four
             else if(s.charAt(i) == ')') {
-                String op = operators.pop();
+                String op = operators.pop();     //pop, evaluate and push result if token is ")"
                 double v = operands.pop();
                 if(op.equals("+")) v = operands.pop() + v;
                 else if(op.equals("-") ) v = operands.pop() - v;
                 else if(op.equals("*")) v = operands.pop() * v;
                 else if(op.equals("/")) {
-                    if(v == 0) throw new RuntimeException("Division by zero not possible");
+                    if(v == 0) throw new RuntimeException("Division by zero not possible");  //if denominator equals zero
                     v = operands.pop() / v;
                 }
                 else if(op.equals("sqrt")) v = Math.sqrt(v);
                 operands.push(v);
-            }else if(s.charAt(i) == ' ');
-            else {
-                int pomak = numberLength(s,i);
-                try{
-                    operands.push(Double.parseDouble(s.substring(i, i + pomak)));
-                    i = i + pomak;
-                }catch(NumberFormatException e){
-                    throw new RuntimeException("Input not valid!");
-                }
+            }   else if(s.charAt(i) == ' ');
+                else {
+                    int shift = numberLength(s,i);
+                    try{
+                        operands.push(Double.parseDouble(s.substring(i, i + shift)));    //token not operator or paranthesis, push double value
+                        i = i + shift;
+                    }catch(NumberFormatException e){
+                        throw new RuntimeException("Input not valid!");
+                    }
 
-            }
+                }
         }
         return operands.pop();
     }
